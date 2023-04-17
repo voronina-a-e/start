cf83049ad665998d10e58af39e8b7a5a9783394a
voronina-a-e <sereginanasti@yandex.ru>
2023-04-17 23:24:44 +0300

    hello

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello!");
 
     }
 }
