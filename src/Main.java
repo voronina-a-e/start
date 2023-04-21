13eab6ad88d58534ca3774f088731d97c1cabb6e
voronina-a-e <sereginanasti@yandex.ru>
2023-04-21 18:55:03 +0300

    1. список моделей телефонов 2. вывести все модели на экран 3 сравнить названия моделей из массива между собой. 4, все занесено в методы и вызваны в main

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Shop iphone = new Shop();
         iphone.modeliphone = new String[]{"iPhoneXS", "iPhoneXR", "iPhoneSE", "iPhone11", "iPhone11Pro", "iPhone11ProMax", "iPhone12", "iPhone12Pro", "iPhone12ProMax", "iPhone13", "iPhone13Pro", "iPhone13ProMax", "iPhone14", "iPhone14Pro", "iPhone14ProMax"};
-        // 1. список моделей телефонов.
-        System.out.println("В магазине всего: " + iphone.modeliphone.length + " моделей iPhone");
 
+        //1. список моделей телефонов.
+        iphone.listModels(iphone.modeliphone);
         //2. вывести все модели на экран.
-        for (int x = 0; x < iphone.modeliphone.length; x++) {
-            System.out.println(iphone.modeliphone[x]);
-        }
-
+        iphone.сycle(iphone.modeliphone);
         //3 сравнить названия моделей из массива между собой.
-        for (int n = 1; n < iphone.modeliphone.length; n++) {
-            if (iphone.modeliphone[n].equalsIgnoreCase(iphone.modeliphone[0]))
-                System.out.println("Есть одинаковые модели: " + iphone.modeliphone[n]);
-        } //или создать новый массив и сравнить Arrays.equals(modeliphone, новый массив)
-
+        iphone.comparisonModels(iphone.modeliphone);
         // - вывести все модели, у которых название длиннее 10 символов
-        //  - из существующего массива телефонов создать новый, в котором у всех букв будет изменен регистр.
-
-        String[] modeliphone2;
-        modeliphone2 = Arrays.copyOf(iphone.modeliphone,15);
-        System.out.println(Arrays.toString(modeliphone2));// - вывод на экран нового массива
+        //из существующего массива телефонов создать новый, в котором у всех букв будет изменен регистр.
+        iphone.newArray(iphone.modeliphone);
 
     }
 }
--- src/Shop.java
+++ src/Shop.java
@@ -1,7 +1,34 @@
+import java.util.Arrays;
+
 public class Shop {
-
-
     public String[] modeliphone;
-    public String[] modeliphone2;
+    public String[] modeliphone2;// почему?
 
+    //создаю метод, обрабатывающий
+    public void сycle(String[] model) {
+        for (int x = 0; x < model.length; x++) {
+            System.out.println(model[x]);
+        }
+    }
+
+    public void comparisonModels(String[] model){
+        for (int n = 1; n < model.length; n++) {
+            if (model[n].equalsIgnoreCase(model[0]))
+                System.out.println("Есть одинаковые модели: " + model[n]);
+            else
+                System.out.println("модель: " + model[n] + " отличается от модели: " + model[0]);
+
+        }
+    }
+    ////или создать новый массив и сравнить Arrays.equals(modeliphone, новый массив)
+
+    public void newArray(String[] model){
+        String[] modeliphone2;
+        modeliphone2 = Arrays.copyOf(model,15);
+        System.out.println(Arrays.toString(modeliphone2));// - вывод на экран нового массива
+    }
+
+    public void listModels(String[] model){
+        System.out.println("В магазине всего: " + model.length + " моделей iPhone");
+    }
 }
