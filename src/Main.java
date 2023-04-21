318b6a7162dc7651b242b807568376e0da7bc63c
voronina-a-e <sereginanasti@yandex.ru>
2023-04-21 17:28:52 +0300

    1. список моделей телефонов 2. вывести все модели на экран 3 сравнить названия моделей из массива между собой.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,32 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello!");
+        Shop iphone = new Shop();
+        iphone.modeliphone = new String[]{"iPhoneXS", "iPhoneXR", "iPhoneSE", "iPhone11", "iPhone11Pro", "iPhone11ProMax", "iPhone12", "iPhone12Pro", "iPhone12ProMax", "iPhone13", "iPhone13Pro", "iPhone13ProMax", "iPhone14", "iPhone14Pro", "iPhone14ProMax"};
+        // 1. список моделей телефонов.
+        System.out.println("В магазине всего: " + iphone.modeliphone.length + " моделей iPhone");
+
+        //2. вывести все модели на экран.
+        for (int x = 0; x < iphone.modeliphone.length; x++) {
+            System.out.println(iphone.modeliphone[x]);
+        }
+
+        //3 сравнить названия моделей из массива между собой.
+        for (int n = 1; n < iphone.modeliphone.length; n++) {
+            if (iphone.modeliphone[n].equalsIgnoreCase(iphone.modeliphone[0]))
+                System.out.println("Есть одинаковые модели: " + iphone.modeliphone[n]);
+        } //или создать новый массив и сравнить Arrays.equals(modeliphone, новый массив)
+
+        // - вывести все модели, у которых название длиннее 10 символов
+        //  - из существующего массива телефонов создать новый, в котором у всех букв будет изменен регистр.
+
+        String[] modeliphone2;
+        modeliphone2 = Arrays.copyOf(iphone.modeliphone,15);
+        System.out.println(Arrays.toString(modeliphone2));// - вывод на экран новый массив
+        for (x=0; x<modeliphone2.length; x++);{
+            System.out.println(modeliphone2[x].toLowerCase());
+        }
 
     }
 }
---
+++ src/Shop.java
@@ -0,0 +1,7 @@
+public class Shop {
+
+
+    public String[] modeliphone;
+    public String[] modeliphone2;
+
+}
